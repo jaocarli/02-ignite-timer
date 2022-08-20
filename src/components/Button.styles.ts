28a884b1d11732e5ae01b109c6b834dc8c6ee14a
João Carli <46573038+jaocarli@users.noreply.github.com>
2022-08-20 16:57:20 -0300

    [feat] - Adicionado o tema da aplicação

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,7 +20,8 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-300']};
 
   /* ${(props) => {
     return css`
